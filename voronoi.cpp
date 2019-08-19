e859922ca3f5700043b101bdbc8791ed0c1c7c6f
Dvij Kalaria <dvij.kalaria@gmail.com>
2019-08-19 20:11:57 +0530

    Variable namechange, code cleaned

--- voronoi.cpp
+++ voronoi.cpp
@@ -10,46 +10,53 @@ using namespace std;
 #include<queue>
 
 
-int main()
+int main(int argv,char** argc)
 {
-	Mat d=imread("Binary1.png",0);
-	imshow("a",d);
 	
-	Mat in(d.rows,d.cols,CV_8UC1,Scalar(0));
+	Mat input=imread(argc[1],0);
+	Mat input_border(input.rows,input.cols,CV_8UC1,Scalar(0));
 	int i,j;
-	for(i=0;i<d.rows;i++)
+	
+	//To binarise the image
+	for(i=0;i<input.rows;i++)
 	{
-		for(j=0;j<d.cols;j++)
+		for(j=0;j<input.cols;j++)
 		{
-			if(d.at<uchar>(i,j)>30)
-				d.at<uchar>(i,j)=255;
+			if(input.at<uchar>(i,j)>30)
+				input.at<uchar>(i,j)=255;
 			else
-				d.at<uchar>(i,j)=0;
+				input.at<uchar>(i,j)=0;
 		}
 	}
-	Mat a=d.clone();
-	for(i=0;i<a.rows;i++)
+	
+	//To invert the image(comment this section if it is not required to be inverted i.e. if the obstacles are already black)
+//////////////////////////////////////////////////////////////////////////////////////////
+	/*
+	Mat input_inverted=input.clone();
+	for(i=0;i<input_inverted.rows;i++)
 	{
-		for(j=0;j<a.cols;j++)
+		for(j=0;j<input_inverted.cols;j++)
 		{
-			if(d.at<uchar>(i,j)==255)
-				d.at<uchar>(i,j)=0;
+			if(input.at<uchar>(i,j)==255)
+				input.at<uchar>(i,j)=0;
 			else
-				d.at<uchar>(i,j)=255;
+				input.at<uchar>(i,j)=255;
 		}
 	}
-	for(i=1;i<d.rows-1;i++)
+	*/
+///////////////////////////////////////////////////////////////////////////////////////////
+	
+	// To generate input image which consists of white pixels representing borders of the obstacle objects as input_border the original image
+	for(i=1;i<input.rows-1;i++)
 	{
-		for(j=1;j<d.cols-1;j++)
+		for(j=1;j<input.cols-1;j++)
 		{	
-			//cout<<((int)d.at<uchar>(i,j)==0)<<"   "<<endl;
-			if((int)d.at<uchar>(i,j)==0)
+			
+			if((int)input.at<uchar>(i,j)==0)
 			{
-				//cout<<"yes"<<endl;
-				//cout<<(int)d.at<uchar>(i,j)<<"    "<<endl;
-	            if((int)d.at<uchar>(i+1,j)==255||(int)d.at<uchar>(i+1,j+1)==255||(int)d.at<uchar>(i-1,j)==255||(int)d.at<uchar>(i,j-1)==255||(int)d.at<uchar>(i,j+1)==255||(int)d.at<uchar>(i-1,j-1)==255||(int)d.at<uchar>(i+1,j-1)==255||(int)d.at<uchar>(i-1,j+1)==255)
-				{	//cout<<"jckbkjdck"<<endl;
-						in.at<uchar>(i,j)=255;
+				if((int)input.at<uchar>(i+1,j)==255||(int)input.at<uchar>(i+1,j+1)==255||(int)input.at<uchar>(i-1,j)==255||(int)input.at<uchar>(i,j-1)==255||(int)input.at<uchar>(i,j+1)==255||(int)input.at<uchar>(i-1,j-1)==255||(int)input.at<uchar>(i+1,j-1)==255||(int)input.at<uchar>(i-1,j+1)==255)
+				{	
+					input_border.at<uchar>(i,j)=255;
 				}
 			}
 		}
@@ -58,23 +65,13 @@ int main()
 
 	Mat output_colored;
 	Mat pushed;
-	main_bfs(in);
-	Mat c(a.rows,a.cols,CV_8UC1,Scalar(0));
-	c=draw_main_bfs(a,a);
-
-
-
-
-
-
-
-
-
-	 
-	imshow("d",a);
-
-	imshow("dwc",c);
-	waitKey(000);
+	main_bfs(input_border);
+	Mat output(input.rows,input.cols,CV_8UC1,Scalar(0));
+	output=draw_main_bfs(input,input);
+	imshow("Input",input);
+	imshow("Input with Borders",input_border);
+	imshow("Output",output);
+	waitKey(0);
 
 	
 }
